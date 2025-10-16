a7fb4f809fc0ad21b17cd2d30e88fca5042c2902
SiD-20s <sidhusiddharth2020@gmail.com>
2025-10-16 14:36:59 +0530

    Revert "fix device responsiveness"
    This reverts commit f4777123c538cc6292c0b3577a152cc7af6c905f.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
-  "/",
-  "/aboutus",
-  "/contactus"
+  "/"
 ])
 
 export default clerkMiddleware(async (auth, req) => {
